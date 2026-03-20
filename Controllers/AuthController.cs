938d1d629c5f9f6848dc3f6d138c138ab91c67b4
KONARA SANJANA <chamodibit2022@gmail.com>
2026-03-21 02:25:10 +0530

    implement Login endpoint

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.DotNet.Scaffolding.Shared.Messaging;
 using Microsoft.EntityFrameworkCore;
 using SchoolManagementSystem.Data;
 using SchoolManagementSystem.DTOs;
@@ -19,6 +20,47 @@ namespace SchoolManagementSystem.Controllers
         {
             _context = context;
             _jwtService = jwtService;
+        }
+
+        // POST api/auth/login
+        // Body: { "userName": "admin01", "password": "secret123"}
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequestDto dto)
+        {
+            // Step 1: Find the user by username
+            var user = await _context.Users
+                .FirstOrDefaultAsync(u => u.UserName == dto.UserName);
+
+                // Step 2 : If user not found, return error
+                if (user == null)
+                return Unauthorized(new { message = "Invalid username or password."});
+
+                // Step 3 : Check if account is acctive
+                if (user.Status == UserStatus.Inactive)
+                    return Unauthorized(new {message = "Account is disabled. Contact admin."});
+
+                // Step 4 : Verify the password using BCryppt
+                bool passwordValid = BCrypt.Net.BCrypt.Verify(dto.Password, user.Password);
+                if (!passwordValid)
+                    return Unauthorized(new {message = "Invalid username or password."});
+
+             //Step 5: Generate JWT token
+             var (token, expiresAt) = _jwtService.GenerateToken(user);
+
+             // Step 6: Return the response
+             return Ok(new AuthResponseDto
+             {
+                 UserId = user.UserId,
+                 UserName = user.UserName,
+                 Email = user.Email,
+                 Role = user.Role.ToString(),
+                 Token = token,
+                 ExpiresAt = expiresAt
+             });
+
+
+            
         }
 
 
