8f594f8db0d86a89451dc7eaed6a5f962a9b51c4
KONARA SANJANA <chamodibit2022@gmail.com>
2026-03-26 13:49:46 +0530

    add Interfaces & Repositories Folders

--- obj/Debug/net9.0/SchoolManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net9.0/SchoolManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f83ab99b24acab550f615b5d2c4f148e136ac0d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4d95df9f66f597ae864157e5c1657ea113886ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
